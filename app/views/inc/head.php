d40fc2a034cde62a0eeff91f1a22853eb955932b
Gerard Merchant <gerardmerchant117@gmail.com>
2024-10-27 21:18:53 -0600

    Comienzo del proyecto, se creo arbol de directorio, modelo y controlador de las vistas, ademas de acregar algunas paginas y navbar.

---
+++ app/views/inc/head.php
@@ -0,0 +1,38 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+<link rel="apple-touch-icon" href="<?php echo APP_URL; ?>app/views/img/apple-icon.png">
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo APP_URL; ?>app(views/img/favicon.ico">
+
+<!-- Bootstrap css-->
+<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/bootstrap.min.css">
+
+<!-- DataTables css-->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.12.1/datatables.min.css" />
+
+<!-- CSS of the original resurse -->
+<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/templatemo.css">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views//css/custom.css">
+
+<!-- Load fonts style after rendering the layout styles -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
+<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/fontawesome.min.css">
+
+<!-- sweetalert2 css-->
+<link rel="stylesheet" href="<?php echo APP_URL; ?>app/views/css/sweetalert2.min.css">
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<!-- Bootstrap js-->
+<script src="<?php echo APP_URL; ?>app/views/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+
+<!-- DataTables js-->
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.12.1/datatables.min.js"></script>
+
+<!-- sweetalert2 js-->
+<script src="<?php echo APP_URL; ?>app/views/js/sweetalert2.all.min.js"></script>
+
+
+<title><?php echo APP_NAME; ?></title>
---
+++ config/server.php
@@ -0,0 +1,8 @@
+<?php 
+
+// Se incluyen los datos del servidor.
+
+ const DB_SERVER="localhost";
+ const DB_NAME="rentasm";
+ const DB_USER="root";
+ const DB_PASS="12345678";
